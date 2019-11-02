8611bb280bf0556b9c5915ed4689df0842043e68
Fazalur Rahman <frahman@aligntech.com>
2019-11-02 16:36:00 +0100

    Controller and acceptance test updated

--- tests/acceptance/play-board-test.js
+++ tests/acceptance/play-board-test.js
@@ -1,10 +1,14 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import { visit, currentURL, click } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 
 module('Acceptance | play board', function(hooks) {
   setupApplicationTest(hooks);
 
+  async function buttonClickAcceptance(assert, element, position, expectedValue) {
+    await click(`#square${position}`);
+    assert.equal(element.querySelector('#square'+position).textContent, expectedValue)
+  }
 
   test('should show board as the home page', async function (assert) {
     await visit('/');
@@ -12,9 +16,29 @@ module('Acceptance | play board', function(hooks) {
   });
 
   test('should show buttons on board as the home page', async function (assert) {
+
     await visit('/');
     assert.equal(this.element.querySelectorAll('button')[0].textContent, "Start New Game With X")
     assert.equal(this.element.querySelectorAll('button')[1].textContent, "Start New Game With O")
+
+  });
+
+  test('should show winner successfully', async function (assert) {
+
+    await visit('/');
+
+    await click('button','Start New Game With X');
+
+    await buttonClickAcceptance(assert, this.element, 0, 'X')
+    await buttonClickAcceptance(assert, this.element, 1, 'O')
+    await buttonClickAcceptance(assert, this.element, 3, 'X');
+    await buttonClickAcceptance(assert, this.element, 4, 'O');
+    await buttonClickAcceptance(assert, this.element, 8, 'X');
+    await buttonClickAcceptance(assert, this.element, 7, 'O');
+    await buttonClickAcceptance(assert, this.element, 5, '');
+    
+    //Check winner 
+    assert.equal(this.element.querySelector('#winner_result').textContent, 'O')
 
   });
 
--- tests/unit/controllers/play-board-test.js
+++ tests/unit/controllers/play-board-test.js
@@ -5,46 +5,44 @@ import EmberObject from '@ember/object';
 module('Unit | Controller | play-board', function(hooks) {
   setupTest(hooks);
 
-  function buttonClickModelTest(assert, controller, position, currentPlayer, winner, nextPlayer, playerXScore, playerOScore) {
-    controller.send('buttonClick', position);
-    assert.equal(controller.get('model.board')[position], currentPlayer, `position ${position} filled with ${currentPlayer}`);
+  function buttonClickModelTest(assert, controller, action, position, currentPlayer, winner, nextPlayer, playerXScore, playerOScore) {
+    controller.send(action, (position != null ? position : currentPlayer));
+    if (position != null ) {
+      assert.equal(controller.get('model.board')[position], currentPlayer, `position ${position} filled with ${currentPlayer}`);
+    } else {
+      assert.equal(controller.get('model.board').join(','), Array(9).fill(null).join(','), 'Empty Board');
+    }
     assert.equal(controller.get('model.winner'), winner, `Winner ${winner}`);
     assert.equal(controller.get('model.nextPlayer'), nextPlayer, `Next Player ${nextPlayer}`);
     assert.equal(controller.get('model.playerXScore'), playerXScore, `Player X Score ${playerXScore}`);
     assert.equal(controller.get('model.playerOScore'), playerOScore, `Player O Score ${playerOScore}`);
   }
 
-  // Replace this with your real tests.
+  // Test the Controller actions.
   test('it exists', function(assert) {
     let controller = this.owner.lookup('controller:play-board');
     assert.ok(controller);
 
+    //set model
     controller.set('model', EmberObject.create({ board: Array(9).fill(null),
       nextPlayer: 'X',
       winner: null,
       playerXScore: 0,
       playerOScore: 0 }));
 
-    controller.send('newGame', 'X');
-    assert.equal(controller.get('model.winner'), null, 'Empty winner');
-    assert.equal(controller.get('model.nextPlayer'), 'X', 'Next Player X');
-    assert.equal(controller.get('model.board').join(','), Array(9).fill(null).join(','), 'Empty Board');
-    assert.equal(controller.get('model.playerXScore'), 0, 'Player X Score 0');
-    assert.equal(controller.get('model.playerOScore'), 0, 'Player O Score 0');
 
+    // newGame Action
+    buttonClickModelTest(assert, controller, 'newGame', null, 'X', null, 'X', 0, 0);
 
-    buttonClickModelTest(assert, controller, 0, 'X', null, 'O', 0, 0);
-    buttonClickModelTest(assert, controller, 1, 'O', null, 'X', 0, 0);
-    buttonClickModelTest(assert, controller, 3, 'X', null, 'O', 0, 0);
-    buttonClickModelTest(assert, controller, 4, 'O', null, 'X', 0, 0);
-    buttonClickModelTest(assert, controller, 6, 'X', 'X', 'O', 1, 0);
+    // board buttonClick Action    
+    buttonClickModelTest(assert, controller, 'buttonClick', 0, 'X', null, 'O', 0, 0);
+    buttonClickModelTest(assert, controller, 'buttonClick', 1, 'O', null, 'X', 0, 0);
+    buttonClickModelTest(assert, controller, 'buttonClick', 3, 'X', null, 'O', 0, 0);
+    buttonClickModelTest(assert, controller, 'buttonClick', 4, 'O', null, 'X', 0, 0);
+    buttonClickModelTest(assert, controller, 'buttonClick', 6, 'X', 'X', 'O', 1, 0);
 
-    controller.send('reset');
-    assert.equal(controller.get('model.winner'), null, 'Empty winner');
-    assert.equal(controller.get('model.nextPlayer'), '', 'Next Player ');
-    assert.equal(controller.get('model.board').join(','), Array(9).fill(null).join(','), 'Empty Board');
-    assert.equal(controller.get('model.playerXScore'), 0, 'Player X Score 0');
-    assert.equal(controller.get('model.playerOScore'), 0, 'Player O Score 0');
+    // reset Action
+    buttonClickModelTest(assert, controller, 'reset', null, 'X', null, '', 0, 0);
 
   });
 });
